73830d447ebd1ff763637cd3f1b17aa5fa5b2f17
bb569 <rahimihosein807@gmail.com>
2026-08-15 04:55:40 -0700

    secound

---
+++ beginning/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views  # از . استفاده کنید چون در same directory هستید
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.book, name='book'),
+]

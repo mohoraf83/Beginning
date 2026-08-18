a7ac7480bfadaba52abe7ba3cffcc10998b1e032
Your Name <your-email@example.com>
2026-08-18 12:15:46 -0700

    true teilwind

--- beginning/views.py
+++ beginning/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-
-def book(req):
-    return render(req, "pages/book.html")
 def home(req):
     return render(req, "base.html")
+def book(req):
+    return render(req, "pages/book.html")
 def goods(req):
     return render(req,"pages/goods.html")
 
